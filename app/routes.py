8980454fa81a32e7a22de6ebe73aa6cefeb95d6d
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-01 19:41:53 +0100

    get id city as url parameter

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,20 @@
 from flask import render_template
+from flask import request
 from app import app
 
-@app.route('/')
+@app.route("/")
 def index():
-	return "Para testar, por favor acesse as seguintes rotas: /city"
+	return "Para testar, por favor acesse a seguinte rota segundo o exemple: localhost:5000/city?id=<ID_DA_CIDADE>. id=3477 para cidade de São Paulo"
 
 @app.route('/city')
-def city():
-	return render_template("city.html")
+# example: localhost:5000/cidade?id=<ID_DA_CIDADE>
+def success():
+    idd = request.args.get('id')
+    print(idd)
+    return idd
+
+# @app.route("/city", methods=["GET", "POST"])
+# def city():
+#     if request.form:
+#         print(request.form)
+#     return render_template("city.html")
