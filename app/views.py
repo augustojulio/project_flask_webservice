4e06bc248c8c74fc80eedbfda9937ac38117c234
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-02 18:04:46 +0100

    saving partial response (cidade, estado, pais) into DB

--- app/views.py
+++ app/views.py
@@ -3,12 +3,6 @@ from app import models
 from flask import render_template, request, jsonify
 import requests
 
-
-print(models.Forecast)
-
-forecast1 = models.Forecast.create(cidade='Sao Carlos', estado='sp', pais='br', data='aaa', probabilidade=3, precipitacao=3, temperatura_min=33, temperatura_max=38)
-forecast1.save()
-
 @app.route("/")
 def index():
 	return "Para testar, por favor acesse a seguinte rota segundo o exemple: localhost:5000/city?id=ID_DA_CIDADE. Sendo id=3477 para cidade de São Paulo"
@@ -24,7 +18,14 @@ def city():
 
     url = 'http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/{0}/days/15?token={1}'.format(id_city, token)
     forecast = requests.get(url).json()
+    print(forecast["id"])
+    print(forecast["name"])
+    print(forecast["state"])
+    print(forecast["country"])
+    # print(forecast["data"])
+
+    forecast1 = models.Forecast.create(cidade=forecast["name"], estado=forecast["state"], pais=forecast["country"], data='aaa', probabilidade=3, precipitacao=3, temperatura_min=33, temperatura_max=38)
+    forecast1.save()
 
     return jsonify(forecast)
-
 
