e0a575008f93cf10d1b79d96517bb94c25b3884b
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-02 18:50:47 +0100

    saving full response (cidade, estado, pais, data, probabilidade, precipitação, temperatura_min, temperatura_max) into DB - business rule 3 ok

--- app/views.py
+++ app/views.py
@@ -18,14 +18,26 @@ def city():
 
     url = 'http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/{0}/days/15?token={1}'.format(id_city, token)
     forecast = requests.get(url).json()
-    print(forecast["id"])
-    print(forecast["name"])
-    print(forecast["state"])
-    print(forecast["country"])
-    # print(forecast["data"])
+    # print(forecast["id"])
+    # print(forecast["name"])
+    # print(forecast["state"])
+    # print(forecast["country"])
+    for i in forecast["data"]:
+        print(forecast["id"])
+        print(forecast["name"])
+        print(forecast["state"])
+        print(forecast["country"])
+        print(i["date_br"])
+        print(i["rain"]["probability"])
+        print(i["rain"]["precipitation"])
+        print(i["temperature"]["min"])
+        print(i["temperature"]["max"])
 
-    forecast1 = models.Forecast.create(cidade=forecast["name"], estado=forecast["state"], pais=forecast["country"], data='aaa', probabilidade=3, precipitacao=3, temperatura_min=33, temperatura_max=38)
-    forecast1.save()
+        forecast1 = models.Forecast.create(cidade=forecast["name"], estado=forecast["state"], pais=forecast["country"], data=i["date_br"], probabilidade=i["rain"]["probability"], precipitacao=i["rain"]["precipitation"], temperatura_min=i["temperature"]["min"], temperatura_max=i["temperature"]["max"])
+        forecast1.save()
+
+    # forecast1 = models.Forecast.create(cidade=forecast["name"], estado=forecast["state"], pais=forecast["country"], data='aaa', probabilidade=3, precipitacao=3, temperatura_min=33, temperatura_max=38)
+    # forecast1.save()
 
     return jsonify(forecast)
 
