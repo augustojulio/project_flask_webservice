61300efd0fd0630f49574a2c1764fc1610970413
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-02 17:16:41 +0100

    saving objects into DB from view class

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app
-# from models import Forecast
+from app import models
 from flask import render_template, request, jsonify
 import requests
+
+
+print(models.Forecast)
+
+forecast1 = models.Forecast.create(cidade='Sao Carlos', estado='sp', pais='br', data='aaa', probabilidade=3, precipitacao=3, temperatura_min=33, temperatura_max=38)
+forecast1.save()
 
 @app.route("/")
 def index():
@@ -13,13 +19,12 @@ def city():
     id_city = request.args.get('id')
     token = 'b22460a8b91ac5f1d48f5b7029891b53'
 
-    print(id_city)
-    print(token)
+    # print(id_city)
+    # print(token)
 
     url = 'http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/{0}/days/15?token={1}'.format(id_city, token)
     forecast = requests.get(url).json()
 
     return jsonify(forecast)
-
 
 
