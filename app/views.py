ca5bad2c57e23656c6d2a6c05b62cb230f5cb8fb
Augusto Julio <augusto.juliogec@gmail.com>
2019-11-03 16:10:10 +0100

    refactor: removed commented prints

--- app/views.py
+++ app/views.py
@@ -14,15 +14,6 @@ def city():
     url = 'http://apiadvisor.climatempo.com.br/api/v1/forecast/locale/{0}/days/15?token={1}'.format(id_city, token)
     forecast = requests.get(url).json()
     for i in forecast["data"]:
-        print(forecast["id"])
-        print(forecast["name"])
-        # print(forecast["state"])
-        # print(forecast["country"])
-        # print(i["date_br"])
-        # print(i["rain"]["probability"])
-        # print(i["rain"]["precipitation"])
-        # print(i["temperature"]["min"])
-        # print(i["temperature"]["max"])
 
         forecast1 = models.Forecast.create(cidade=forecast["name"], estado=forecast["state"], pais=forecast["country"], data=i["date_br"], probabilidade=i["rain"]["probability"], precipitacao=i["rain"]["precipitation"], temperatura_min=i["temperature"]["min"], temperatura_max=i["temperature"]["max"])
         forecast1.save()
@@ -34,28 +25,17 @@ def city():
 def analysis():
     initial_date = request.args.get('data_inicial')
     final_date = request.args.get('data_final')
-    # print(initial_date)
-    # print(final_date)
 
     list_results = []
 
-    # forecasts2 = models.Forecast.select().where(models.Forecast.data.between(initial_date, final_date))
-    # for forecast2 in forecasts2:
-    #     print('{} , {} , {} , {}'.format(forecast2.cidade, forecast2.data, forecast2.temperatura_max, forecast2.precipitacao))
-
     forecasts_max = models.Forecast.select(models.Forecast.cidade, models.Forecast.data, fn.MAX(models.Forecast.temperatura_max)).where(models.Forecast.data.between(initial_date, final_date))
     for forecast_max in forecasts_max:
-        # print('A cidade com maior temperatura máxima no período espeficado é: {}'.format(forecast_max.cidade))
         list_results.append('A cidade com maior temperatura máxima no período espeficado é: {}'.format(forecast_max.cidade))
 
     forecasts_avg = models.Forecast.select(models.Forecast.cidade, models.Forecast.data, fn.AVG(models.Forecast.precipitacao).alias('avg_precipitacao')).where(models.Forecast.data.between(initial_date, final_date)).group_by(models.Forecast.cidade)
     for forecast_avg in forecasts_avg:
-        # print('A média de precipitação da cidade de {} é {}'.format(forecast_avg.cidade, forecast_avg.avg_precipitacao))
         list_results.append('A média de precipitação da cidade de {} é {}'.format(forecast_avg.cidade, forecast_avg.avg_precipitacao))
-
-    # print(list_results)
     
     list_results_string = '\n'.join(list_results)
-    # print(list_results_string)
 
     return list_results_string
