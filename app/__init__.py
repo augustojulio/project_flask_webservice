7b76f9d304a54117018298001abf91684d746b84
Augusto Julio <augusto.juliogec@gmail.com>
2019-12-02 16:40:40 -0200

    refactor: model

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,18 @@
 import os
 from flask import Flask
-# from peewee import SqliteDatabase
-from flask_sqlalchemy_core import FlaskSQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-# from sqlalchemy import create_engine
-# engine = create_engine
 
 APP_ROOT = os.path.dirname(os.path.realpath(__file__))
-# DATABASE = os.path.join(APP_ROOT, 'forecasts.db')
-DATABASE_URL = "postgresql://localhost/forecasts"
+
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/forecasts'
 
-# db = SqliteDatabase(app.config['DATABASE'], pragmas={
-#     'journal_mode': 'wal',  # WAL-mode for better concurrent access.
-#     'cache_size': -32000})  # 32MB page cache.
-
-db = FlaskSQLAlchemy(DATABASE_URL)
+db = SQLAlchemy(app)
 
 from app import views
 from app import models
--- app/models.py
+++ app/models.py
@@ -1,18 +1,14 @@
 from app import app, db
-from peewee import *
+from sqlalchemy import Column, Integer, String, DateTime
 
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-class Forecast(BaseModel):
-    cidade = CharField()
-    estado = CharField()
-    pais = CharField()
-    data = DateTimeField()
-    probabilidade = IntegerField()
-    precipitacao = IntegerField()
-    temperatura_min = IntegerField()
-    temperatura_max = IntegerField()
-
-
+class Forecast(db.Model):
+    __tablename__ = 'forecasts'
+    id = db.Column(db.Integer, primary_key=True)
+    cidade = db.Column(db.String(128), unique=True, nullable=False)
+    estado = db.Column(db.String(128), unique=True, nullable=False)
+    pais = db.Column(db.String(128), unique=True, nullable=False)
+    data = db.Column(db.DateTime, nullable=False)
+    probabilidade = db.Column(Integer, unique=True, nullable=False)
+    precipitacao = db.Column(Integer, unique=True, nullable=False)
+    temperatura_min = db.Column(Integer, unique=True, nullable=False)
+    temperatura_max = db.Column(Integer, unique=True, nullable=False)
