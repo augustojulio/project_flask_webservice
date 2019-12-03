6cb9f01de61aeda95ef4c8d7a3685b2ee24e023b
Augusto Julio <augusto.juliogec@gmail.com>
2019-12-03 17:53:32 -0200

    feat: unit tests

---
+++ app/tests/test_one.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestOne(unittest.TestCase):
+
+    # Returns True or False.
+    def test(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ app/tests_r.py
@@ -0,0 +1,27 @@
+
+import os
+import unittest
+from app import app
+class RoutingTests(unittest.TestCase):
+    # function to set up testing connection
+    def home(self):
+         app.config["TESTING"] = True
+         app.config["DEBUG"] = True
+         self.app = app.test_client()
+         self.assertEqual(app.debug,False)
+         response = self.app.get('/', follow_redirects=True)
+         self.assertEqual(response.status_code, 200)
+    # function to teardown connection after testing
+    #  def tear_down():
+    #       pass
+    #  def homepage():
+    #       response = self.app.get('/', follow_redirects = True)
+    #       self.assertEqual(response.status_code, 200)
+    # def aboutpage():
+    #       response = self.app.get('/', follow_redirects = True)
+    #       self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
